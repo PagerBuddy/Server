b231eef6297066a34e2c0a1ec64490bccd86faf4
maxbartunik <42568983+maxbartunik@users.noreply.github.com>
2022-09-25 22:18:36 +0200

    Default testalert to never
    We need to use a nonexistent day as default to enable 'never'

--- src/model/zvei.mjs
+++ src/model/zvei.mjs
@@ -7,11 +7,11 @@ export default class ZVEI {
      * 
      * @param {string|number} zvei_id 
      * @param {string} [description = ""]
-     * @param {number} [test_day = 0]
+     * @param {number} [test_day = -1]
      * @param {string} [test_time_start = "00:00"]
      * @param {string} [test_time_end = "00:00"]
      */
-    constructor(zvei_id, description = "", test_day = 0, test_time_start = "00:00", test_time_end = "00:00") {
+    constructor(zvei_id, description = "", test_day = -1, test_time_start = "00:00", test_time_end = "00:00") {
         
         const validated_id = ZVEI.validate_zvei_id(zvei_id)
         if (!validated_id.isPresent()) {
@@ -24,7 +24,7 @@ export default class ZVEI {
         }
         this.description = description;
 
-        if (test_day < 0 || test_day >= 7) {
+        if (test_day < -1 || test_day >= 7) {
             throw new Error(`Test day '${test_day}' is not from the range [0,6]`);
         }
         this.test_day = test_day;
