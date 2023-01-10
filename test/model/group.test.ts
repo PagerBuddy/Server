4fe70dc8b0be9e5fa6d1edd8053521045b0632b9
Oliver Keszöcze <oliver.keszoecze@fau.de>
2023-01-10 17:22:32 +0100

    try using in-memory sqlite for testing

--- test/model/group.test.ts
+++ test/model/group.test.ts
@@ -1,12 +1,18 @@
 import { describe, expect, test, beforeAll, afterAll, jest } from '@jest/globals'
 import Group from '../../src/model/group'
-
+import { TestHelper } from '../testhelper'
 
 describe("Some Text", () => {
 
-
+    beforeAll(async () => {
+        await TestHelper.instance.setupTestDB();
+    });
+    
+    afterAll(() => {
+        TestHelper.instance.teardownTestDB();
+    });
     test("Create a Group object: %s", () => {
-        //let g1 = new Group()
+        let g1 = Group.create({})
         //let g2 = new Group()
         expect(false).toBeFalsy()
     });
